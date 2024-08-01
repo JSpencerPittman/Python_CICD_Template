ea809b489792c61328df4a44efa6c86ebd16de6e
jspencerpittman <jspencerpittman@gmail.com>
2024-07-31 20:17:20 -0400

    Created source code

---
+++ src/main.py
@@ -0,0 +1,4 @@
+from python import Python
+
+monty = Python('monty')
+monty.Travel((0, 0))
---
+++ src/python.py
@@ -0,0 +1,7 @@
+class Python:
+    def __init__(self, name):
+        self.name = name
+        self.x, self.y = 0, 0
+
+    def Travel(self, pos):
+        self.x, self.y = pos
